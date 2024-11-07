d2ed08eac6e16cdcf5dead6599fb174f4ba3de1b
erikk4 <e.keplinger@students.htl-leonding.ac.at>
2024-11-07 11:07:36 +0100

    #1 implemented circle on click

--- SimTune/src/app/piano/piano.component.ts
+++ SimTune/src/app/piano/piano.component.ts
@@ -10,5 +10,33 @@ import { RouterModule } from '@angular/router';
   styleUrl: './piano.component.css'
 })
 export class PianoComponent {
+  selectedKey: string | null = null;
 
+  whiteKeys = [
+    { id: 'c-1' }, { id: 'd-1' }, { id: 'e-1' }, { id: 'f-1' },
+    { id: 'g-1' }, { id: 'a-1' }, { id: 'h1' }, { id: 'c-2' },
+    { id: 'd-2' }, { id: 'e-2' }, { id: 'f-2' }, { id: 'g-2' },
+    { id: 'a-2' }, { id: 'h-2' }
+  ];
+
+  blackKeys = [
+    { id: 'cis-1' },
+    { id: 'dis-1' },
+    { id: 'fis-1' },
+    { id: 'gis-1' },
+    { id: 'ais-1' },
+    { id: 'cis-2' },
+    { id: 'dis-2' },
+    { id: 'fis-2' },
+    { id: 'gis-2' },
+    { id: 'ais-2' }
+  ];
+
+  onKeyClick(keyId: string) {
+    if (this.selectedKey === keyId) {
+      this.selectedKey = null;
+    } else {
+      this.selectedKey = keyId;
+    }
+  }
 }
--- SimTune/tailwind.config.js
+++ SimTune/tailwind.config.js
@@ -10,6 +10,7 @@ module.exports = {
         costumBlue: '#6496FF',
         costumLightBlue: 'rgba(100, 150, 255, 0.25)',
         costumLightGray: '#F0F0F0',
+        costumDarkGray: '#747171',
       },
       boxShadow: {
         'even': '0 0 10px 1px rgba(0, 0, 0, 0.5)',
