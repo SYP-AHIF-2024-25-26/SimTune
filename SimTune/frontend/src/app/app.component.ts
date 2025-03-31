6572a101bbf3701b4937c4d150143655321844d2
Tobias <t.vieghofer@students.htl-leonding.ac.at>
2025-03-31 08:21:00 +0200

    #36 Volume btn bug fix

--- SimTune/frontend/src/app/app.component.ts
+++ SimTune/frontend/src/app/app.component.ts
@@ -11,12 +11,23 @@ import { FormsModule } from '@angular/forms';
 })
 export class AppComponent {
   volume: number = 100;
+  lastVolume: number = 100;
 
   ngOnInit(): void {
     this.volume = parseInt(sessionStorage.getItem('volume') ?? '100');
+    this.lastVolume = this.volume;
   }
 
   adjustVolume(): void {
     sessionStorage.setItem('volume', this.volume.toString());
+    this.lastVolume = this.volume;
+  }
+
+  noSound(): void {
+    if(this.volume == 0) {
+      this.volume = this.lastVolume;
+    } else {
+      this.volume = 0;
+    }
   }
 }
