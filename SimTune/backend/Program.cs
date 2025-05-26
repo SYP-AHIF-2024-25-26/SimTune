9e55d313d60602f6696b5973e9f7249c011ad9aa
erikk4 <e.keplinger@students.htl-leonding.ac.at>
2025-05-26 09:50:19 +0200

    Update Program.cs

--- SimTune/backend/Program.cs
+++ SimTune/backend/Program.cs
@@ -16,7 +16,6 @@ if (builder.Environment.IsDevelopment())
 else
 {
     builder.Configuration.AddJsonFile("appsettings.json", optional: true, reloadOnChange: false);
-    Console.WriteLine("Verwende DB: " + builder.Configuration["AppUrl"]);
 }
 
 builder.Configuration.AddEnvironmentVariables();
