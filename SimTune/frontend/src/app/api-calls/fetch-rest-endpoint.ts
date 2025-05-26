0c8338c5bc17b0b800aae6fe333946d68124c7c5
erikk4 <e.keplinger@students.htl-leonding.ac.at>
2025-05-26 10:49:58 +0200

    change environment variables setup in frontend

--- SimTune/frontend/src/app/api-calls/fetch-rest-endpoint.ts
+++ SimTune/frontend/src/app/api-calls/fetch-rest-endpoint.ts
@@ -1,9 +1,13 @@
+import { environment } from "../../environments/environment";
+
 export { fetchRestEndpoint, API_URL, fetchRestEndpointWithAuthorization };
   // für lokale Entwicklung
   //const API_URL = "http://localhost:5069/";
 
   // für leocloud
-  const API_URL = "https://if210019.cloud.htl-leonding.ac.at/api/";
+  //const API_URL = "https://if210019.cloud.htl-leonding.ac.at/api/";
+
+  const API_URL = environment.apiUrl;
 
 
   async function fetchRestEndpoint
